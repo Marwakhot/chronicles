ff79f683dc106f0b6481115422cf447ba5845105
Marwa Khot <marwakhot007@gmail.com>
2025-10-26 14:38:48 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: [],
   },
+  // Add Netlify-specific config
+  trailingSlash: true,
+  target: 'serverless'
 }
 
 module.exports = nextConfig
